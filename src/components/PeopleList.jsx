8d3068631d023d84836b58ba190cda4d838eea7f
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-22 13:01:43 +0200

    uzd

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -2,12 +2,21 @@ import Person from './Person';
 
 const PeopleList = (props) => {
   // mapinti per gauta sarasa ir gamiti Person componentus
+  const zmSk = 'gauti reiksme is props';
+  const hobiai = 'gauti reiksme is props';
   return (
-    <ul>
-      {props.list.map((personObj) => (
-        <Person key={personObj.id} name={personObj.name} />
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {props.list.map((personObj) => (
+          <Person key={personObj.id} name={personObj.name} />
+        ))}
+      </ul>
+
+      <div className='info'>
+        <p>Viso zmoniu: XX</p>
+        <p>Visi zmoniu hobiai: reading, swimming ...</p>
+      </div>
+    </div>
   );
 };
 export default PeopleList;
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,11 +1,16 @@
 import UiCard from './ui/UiCard';
 
 const Person = (props) => {
+  // atvaizduoti visa info is objekto
+  // kur lytis parasyti "vyras" arba "moteris"
+  // <button>Dark Mode</button> paspaudus uzdesim klase li elementui ir fonas taps tamsus , textas sviesus
+
   return (
     <li>
       <UiCard>
         <h3>{props.name}</h3>
         <p>Age: </p>
+        <button>Dark Mode</button>
       </UiCard>
     </li>
   );
