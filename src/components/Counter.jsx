f64b1bf6c7dd3f0c4d4d966bcce044f8779c1fe8
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-22 10:55:08 +0200

    counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import UiCard from './ui/UiCard';
 
 const Counter = () => {
   console.log('Counter mounted');
   // logic
-  // paprastas kinamasis neveikia jei reikia kintamos reikmes JSX
-  let count = 0;
+
+  // const [Kintamojo pavadinimas, funkcija nustatyti naujai reiksmei] = useState(pradine kintamojo reikme);
+  let [count, setCount] = useState(0);
 
   const handleIncrement = () => {
     console.log('handleIncrement');
-    count = count + 1;
-    console.log('count ===', count);
+    // kai naujas state priklauso nuo pries tai buvusio turetume atnaujinti su funkcija
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    //
+  };
+
+  const handleChange = (howMuch) => {
+    //
   };
 
   return (
@@ -17,6 +27,10 @@ const Counter = () => {
       <h3>Counter</h3>
       <h2>{count}</h2>
       <button onClick={handleIncrement}>Plus</button>
+      <button>Minus</button>
+      <button>Add 5</button>
+      <button>Add 10</button>
+      <button>Reset Counter</button>
     </UiCard>
   );
 };
