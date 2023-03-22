33f40a8dd98a6c306a730a42342e221013e54cca
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-22 15:52:05 +0200

    filtered

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -1,19 +1,30 @@
+import { useState } from 'react';
 import Person from './Person';
 
 const PeopleList = (props) => {
   // mapinti per gauta sarasa ir gamiti Person componentus
-  const zmSk = 'gauti reiksme is props';
+  const zmSk = props.list.length;
   const hobiai = 'gauti reiksme is props';
+  let [filtered, setFiltered] = useState([...props.list]);
+
   return (
     <div>
+      <button>vyrai</button>
+      <button>Moterys</button>
       <ul>
         {props.list.map((personObj) => (
-          <Person key={personObj.id} name={personObj.name} />
+          <Person
+            key={personObj.id}
+            name={personObj.name}
+            age={personObj.age}
+            gender={personObj.gender}
+            hobby={personObj.hobby}
+          />
         ))}
       </ul>
-
+      <br />
       <div className='info'>
-        <p>Viso zmoniu: XX</p>
+        <p>Viso zmoniu: {zmSk}</p>
         <p>Visi zmoniu hobiai: reading, swimming ...</p>
       </div>
     </div>
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -4,12 +4,16 @@ const Person = (props) => {
   // atvaizduoti visa info is objekto
   // kur lytis parasyti "vyras" arba "moteris"
   // <button>Dark Mode</button> paspaudus uzdesim klase li elementui ir fonas taps tamsus , textas sviesus
-
+  const vyrasArMoteris = props.gender === 'female' ? 'Moteris' : 'Vyrs';
   return (
     <li>
       <UiCard>
         <h3>{props.name}</h3>
-        <p>Age: </p>
+        <p>Age: {props.age}</p>
+        <h4>Lytis: {vyrasArMoteris}</h4>
+        <p>
+          Hobis:<em> {props.hobby}</em>
+        </p>
         <button>Dark Mode</button>
       </UiCard>
     </li>
